f2893a4da527c9702beefb256be7ae4ed6c4cf6e
genglan <genglan@nationsky.com>
2014-09-26 18:19:18 +0800

    民生应用服务中心

---
+++ www/js/controllers.js
@@ -0,0 +1,15 @@
+angular.module('starter.controllers', [])
+//个人中心
+.controller('PersonalCtrl', function($scope, $ionicModal, $timeout) {
+})
+//主页
+.controller('HomePageCtrl', function($scope) {
+  $scope.playlists = [
+    { title: 'Reggae', id: 1 },
+    { title: 'Chill', id: 2 },
+    { title: 'Dubstep', id: 3 },
+    { title: 'Indie', id: 4 },
+    { title: 'Rap', id: 5 },
+    { title: 'Cowbell', id: 6 }
+  ];
+})
